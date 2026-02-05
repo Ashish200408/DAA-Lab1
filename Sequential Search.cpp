3e2de0684762f9bb764fc2ac1b0905728b02de27
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-02-05 08:58:06 +0530

    Done

---
+++ Sequential Search.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,key;
+    cout<<"Enter number of elements:";
+    cin>>n;
+
+    int a[n];
+    cout<<"Enter elements:\n";
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+
+    cout<<"Enter elements to search:";
+    cin>>key;
+
+    for(int i=0;i<n;i++){
+        if(a[i]==key){
+            cout<<"Elements found at position "<<i+1;
+            return 0;
+        }
+    }
+
+    cout<<"Elements not found";
+    return 0;
+}
